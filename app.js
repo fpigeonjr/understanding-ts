ed8b1c63d1bac2e1d442953126683785777fc46e
Frank Pigeon <frank.pigeonjr@gmail.com>
2021-05-03 16:25:28 -0500

    core types in action

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log('Your code goes here...');
+console.log('time to get started');
+function add(n1, n2, showResult, resultPhrase) {
+    if (resultPhrase === void 0) { resultPhrase = ''; }
+    var result = n1 + n2;
+    if (showResult && resultPhrase.length > 0) {
+        console.log(resultPhrase + result);
+    }
+    else if (showResult) {
+        console.log(result);
+    }
+    else {
+        return result;
+    }
+}
+var number1 = 5;
+var number2 = 5;
+var printResult = true;
+var resultPhrase = 'Result is: ';
+add(number1, number2, true, resultPhrase);
--- app.ts
+++ app.ts
@@ -1,1 +1,24 @@
-console.log('Your code goes here...')
+console.log('time to get started')
+
+function add(
+  n1: number,
+  n2: number,
+  showResult?: boolean,
+  resultPhrase: string = ''
+) {
+  const result = n1 + n2
+  if (showResult && resultPhrase.length > 0) {
+    console.log(resultPhrase + result)
+  } else if (showResult) {
+    console.log(result)
+  } else {
+    return result
+  }
+}
+
+const number1 = 5
+const number2 = 5
+const printResult = true
+const resultPhrase = 'Result is: '
+
+add(number1, number2, true, resultPhrase)
