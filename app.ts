64352e300a1a46d5cd6380087d853ba949c3ba0e
Frank Pigeon <frank.pigeonjr@gmail.com>
2021-05-03 19:57:37 -0500

    adds tuples

--- app.ts
+++ app.ts
@@ -1,12 +1,20 @@
-const person = {
+const person: {
+  name: string
+  age: number
+  hobbies: string[]
+  role: [number, string] // tuple
+} = {
   name: 'Frank',
   age: 42,
   hobbies: ['sports', 'cooking'],
+  role: [2, 'author'],
 }
 
-let favActivities: string[]
-favActivities = ['Sports']
-console.log(person.name)
+// catches these errors
+// person.role.push('admin') // works
+// person.role[1] = 10 // not valid
+
+console.log(person)
 
 for (const hobby of person.hobbies) {
   console.log(hobby)
