59905cf5f5094f27cd28a39ed4283900b21552a8
Frank Pigeon <frank.pigeonjr@gmail.com>
2021-05-03 20:33:28 -0500

    adds ENUMs

--- app.ts
+++ app.ts
@@ -1,13 +1,26 @@
-const person: {
-  name: string
-  age: number
-  hobbies: string[]
-  role: [number, string] // tuple
-} = {
+// const person: {
+//   name: string
+//   age: number
+//   hobbies: string[]
+//   role: [number, string] // tuple
+// } = {
+//   name: 'Frank',
+//   age: 42,
+//   hobbies: ['sports', 'cooking'],
+//   role: [2, 'author'],
+// }
+
+enum Role {
+  ADMIN,
+  READ_ONLY,
+  AUTHOR,
+}
+
+const person = {
   name: 'Frank',
   age: 42,
   hobbies: ['sports', 'cooking'],
-  role: [2, 'author'],
+  role: Role.ADMIN,
 }
 
 // catches these errors
@@ -18,4 +31,8 @@ console.log(person)
 
 for (const hobby of person.hobbies) {
   console.log(hobby)
+}
+
+if (person.role === Role.ADMIN) {
+  console.log('is admin')
 }
