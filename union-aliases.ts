fcdad33722e29758db9a62535b56cb5fa213da2c
Frank Pigeon <frank.pigeonjr@gmail.com>
2021-05-06 15:34:11 -0500

    adds never return type

--- union-aliases.ts
+++ union-aliases.ts
@@ -14,7 +14,7 @@ function combine(
     result = +input1 + +input2
   } else {
     result = input1.toString() + ' and ' + input2.toString()
-  }
+}
   return result
   // if (resultConversion === 'as-number') {
   //     return +result
